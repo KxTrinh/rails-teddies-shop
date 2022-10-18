0dd6eedd595d7add851b0e0473bb1deb70908660
Kevin Trinh <kxt.trinh@gmail.com>
2022-10-18 16:45:44 +0200

    test

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -8,6 +8,20 @@ Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
 StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
 
+# StripeEvent.configure do |events|
+#   events.subscribe 'checkout.session.completed', StripeCheckoutSessionService.new
+# end
+
 StripeEvent.configure do |events|
-  events.subscribe 'checkout.session.completed', StripeCheckoutSessionService.new
+  events.subscribe 'checkout.session.completed' do |event|
+    order = Order.find_by(checkout_session_id: event.data.object.id)
+    order.update(state: 'paid')
+  end
 end
+
+# class StripeCheckoutSessionService
+#   def call(event)
+#     order = Order.find_by(checkout_session_id: event.data.object.id)
+#     order.update(state: 'paid')
+#   end
+# end
